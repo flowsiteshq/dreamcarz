4053bbec38c2f732c3ab0fcc336d450cb785c2af
Flow-sites <dojoflowus@gmail.com>
2026-09-17 16:40:24 -0400

    Register Zapier lead intake route

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -14,6 +14,7 @@ import { registerGoogleOAuthRoutes } from "../googleOAuth";
 import { registerDreamCarzZoomRoute } from "../zoomOpportunity";
 import { registerMetaLeadAdsWebhook } from "../metaLeadAdsWebhook";
 import { registerMetaLeadAdsRetryRoute } from "../metaLeadAdsRetry";
+import { registerZapierMetaLeadWebhook } from "../zapierMetaLeadWebhook";
 
 function isPortAvailable(port: number): Promise<boolean> {
   return new Promise(resolve => {
@@ -40,6 +41,7 @@ async function startServer() {
   registerStripeWebhook(app);
   registerCoCardWebhook(app);
   registerMetaLeadAdsWebhook(app);
+  registerZapierMetaLeadWebhook(app);
   // Configure body parser with larger size limit for file uploads
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
